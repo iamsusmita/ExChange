8d75f5742be5549ac8bf9ee0672b16f190df486a
iamsusmita <69148747+iamsusmita@users.noreply.github.com>
2023-03-07 05:29:40 +0000

    Added HTML with custom styling

--- app.py
+++ app.py
@@ -1,10 +1,10 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route("/")
 def welcome():
-  return "Welcome to ExChange! ... one stop solution for like minded people."
+  return render_template('home.html')
   
 print(__name__)
 if __name__ == "__main__":
